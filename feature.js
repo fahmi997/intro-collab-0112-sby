95f86bec48602def78dab40cafd1b0a6b659b938
fahmi997 <aardiansyah.fahmii@gmail.com>
2023-09-22 14:35:39 +0700

    feat: create initial feature.js

---
+++ feature.js
@@ -0,0 +1,17 @@
+/**
+ * 1. Use arrow fuction
+ * 2. Use const variable for fuction
+ * 3. Function return data only
+ * 4. Use camelCase for fucntion name
+ */
+
+// Feature#1: Add data
+
+// Feature#2: Show data based on date
+
+// Feature#3: Update data
+
+// Feature#4: Delete data
+
+// Feature#5: Show data: Expense, Income, and left money
+
